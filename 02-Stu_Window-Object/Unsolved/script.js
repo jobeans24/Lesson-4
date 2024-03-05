787d27dec34d75b64d235b39980e292feead92c7
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-03-05 18:47:50 -0500

    adding function comments

--- 02-Stu_Window-Object/Unsolved/script.js
+++ 02-Stu_Window-Object/Unsolved/script.js
@@ -1,12 +1,16 @@
 // TODO: What will the following line of code log?
-console.log(window);
+// will log the window object
+console.log(window); 
 
 // TODO: What will the following line of code log?
+// will log the document object
 console.log(window.document);
 
 // TODO: What will the following line of code log?
+// will log the document object elements
 console.log(document.documentElement);
 
 // TODO: What will the following line of code log?
+// will log the head element of the document object
 console.log(document.head);
 
