7e4bb5268b92921bc88970ef3d89be4dd7336025
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-03-05 19:43:40 -0500

    setting attributes to elements

--- 06-Stu_Setting-Attributes/Unsolved/assets/js/script.js
+++ 06-Stu_Setting-Attributes/Unsolved/assets/js/script.js
@@ -1,6 +1,6 @@
-var site1El = document.querySelector(".site1");
-var site2El = document.querySelector(".site2");
-var site3El = document.querySelector(".site3");
+let site1El = document.querySelector(".site1");
+let site2El = document.querySelector(".site2");
+let site3El = document.querySelector(".site3");
 
 site1El.children[0].textContent = "Site 1";
 site1El.children[1].setAttribute("href", "https://google.com");
@@ -21,3 +21,12 @@ site3El.children[1].children[0].setAttribute("alt", "women working");
 site3El.children[1].children[0].setAttribute("style", "padding:10px;");
 
 // Write Your Code Below
+let h4Elements = document.querySelectorAll("h4");
+
+h4Elements.forEach((h4) => {
+  h4.setAttribute("style", "color:blue; font-size:30px; bold;");
+});
+
+let h1Element = document.querySelector("h1");
+
+h1Element.setAttribute("style", "padding:10px", "margin:0");
