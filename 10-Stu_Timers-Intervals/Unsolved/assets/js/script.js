990bd650574b389da22b26e6af1dd85c2cb53acc
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-03-05 20:56:51 -0500

    timer element

--- 10-Stu_Timers-Intervals/Unsolved/assets/js/script.js
+++ 10-Stu_Timers-Intervals/Unsolved/assets/js/script.js
@@ -8,14 +8,22 @@ var words = message.split(' ');
 function countdown() {
   var timeLeft = 5;
 
-  // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-  var timeInterval = setInterval(function () {
-    //
-    // YOUR CODE HERE
-    //
-  });
-}
 
+  // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+ var timeInterval = setInterval(function() {
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } else {
+      timerEl.textContent = '';
+      clearInterval(timeInterval);
+      displayMessage();
+    }
+  }, 1000);
+}
 // Displays the message one word at a time
 function displayMessage() {
   var wordCount = 0;
