1e5763b6d3984ca99b37922011bf3011d9871cfa
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-03-05 19:19:07 -0500

    changing children

--- 04-Stu_Traverse-DOM/Unsolved/assets/js/script.js
+++ 04-Stu_Traverse-DOM/Unsolved/assets/js/script.js
@@ -5,5 +5,5 @@ var headerDiv = document.getElementById('header');
 
 
 // Change style by accessing style object's properties
-articlesDiv.style.fontSize = '50px';
+articlesDiv.children[0].style.fontSize = '50px';
 headerDiv.children[0].style.color = 'white';
