2e1aa58dd58b1cf2e010deb64e6562712a09689b
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-03-07 20:14:21 -0500

    describing function

--- 18-Stu_Event-Bubbling/Unsolved/assets/js/script.js
+++ 18-Stu_Event-Bubbling/Unsolved/assets/js/script.js
@@ -1,6 +1,8 @@
 // TODO: Which element is the following line of code selecting?
+// this const is selecting the element with the class of "carouselbox"
 const carousel = document.querySelector('.carouselbox');
 // TODO: Which element is the following line of code selecting?
+// this const is selecting the element with the class of "next"
 const next = carousel.querySelector('.next');
 const prev = carousel.querySelector('.prev');
 let index = 0;
@@ -27,21 +29,27 @@ function navigate(direction) {
 }
 
 // TODO: Describe the functionality of the following event listener.
+// This event listener is listening for a click event on the carousel element, and when that event is triggered, it will navigate to the next image in the images array
 carousel.addEventListener('click', function () {
   window.location.href = images[index];
 });
 
 // TODO: Describe the functionality of the following event listener.
+// This event listener is listening for a click event on the next element, and when that event is triggered, it will navigate to the next image in the images array
 next.addEventListener('click', function (event) {
   // TODO: What is the purpose of the following line of code?
+  // this line of code is preventing the event from bubbling up the DOM tree
   event.stopPropagation();
 
   navigate(1);
 });
 
 // TODO: Describe the functionality of the following event listener.
+// This event listener is listening for a click event on the prev element, and when that event is triggered, it will navigate to the previous image in the images array
 prev.addEventListener('click', function (event) {
+
   // TODO: What would happen if we didn't add the following line of code?
+  // if we didn't add this line of code, the event would bubble up the DOM tree
   event.stopPropagation();
 
   navigate(-1);
