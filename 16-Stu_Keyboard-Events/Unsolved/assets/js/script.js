a48bfc5fcf3591589870536e92cca5c35ab98a68
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-03-07 20:10:43 -0500

    keyboard actions

--- 16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
+++ 16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
@@ -1,11 +1,15 @@
 function keydownAction(event) {
   // TODO: Complete keydown function
+  document.querySelector("#status").textContent = "KEYDOWN Event"; // sets the text content of the element with the id "status" to "KEYDOWN Event"
 }
 
 function keyupAction() {
   document.querySelector("#status").textContent = "KEYUP Event";
 }
 
-document.addEventListener("keyup", keyupAction);
+document.addEventListener("keyup", keyupAction); // calls the function keyupAction when the keyup event is triggered
 // TODO: Add Event Listener for "keydown" event
+document.addEventListener("keydown", keydownAction); // calls the function keydownAction when the keydown event is triggered
+
+
 
