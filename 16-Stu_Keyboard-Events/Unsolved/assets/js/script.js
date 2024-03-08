8dde72e273a4fe72a5e0ee42201cfb5f6c0a0911
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-03-07 20:48:59 -0500

    lessons

--- 16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
+++ 16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
@@ -1,10 +1,19 @@
+
+let keyPressedEl = document.querySelector("#key"); // sets the key variable to the element with the id "key"
+let keyCodeEl = document.querySelector("#code"); // sets the code variable to the element with the id "code"
+
 function keydownAction(event) {
   // TODO: Complete keydown function
+  let key = event.key; // sets the key variable to the key property of the event object
+  keyPressedEl.textContent = key; // sets the text content of the element with the id "key" to the value of the key variable
+  let code = event.code; // sets the code variable to the code property of the event object
+  keyCodeEl.textContent = code; // sets the text content of the element with the id "code" to the value of the code variable
+
   document.querySelector("#status").textContent = "KEYDOWN Event"; // sets the text content of the element with the id "status" to "KEYDOWN Event"
 }
 
 function keyupAction() {
-  document.querySelector("#status").textContent = "KEYUP Event";
+  document.querySelector("#status").textContent = "KEYUP Event"; // sets the text content of the element with the id "status" to "KEYUP Event"
 }
 
 document.addEventListener("keyup", keyupAction); // calls the function keyupAction when the keyup event is triggered
--- 20-Stu_Data-Attributes/Unsolved/assets/js/script.js
+++ 20-Stu_Data-Attributes/Unsolved/assets/js/script.js
@@ -1,7 +1,18 @@
 const container = document.querySelector('.container');
 
 container.addEventListener('click', function (event) {
-  const element = event.target;
-
   // TODO: Complete function
+  const element = event.target; // gets the element that triggered the event
+  const elementName = element.getAttribute('data-element'); // gets the value of the data-element attribute of the element that triggered the event
+  const elementIndex = element.getAttribute('data-index'); // gets the value of the data-index attribute of the element that triggered the event
+  const elementStatus = document.querySelector('#status'); // gets the element with the id "status"
+  elementStatus.textContent = `${elementName} - ${elementIndex}`; // sets the text content of the element with the id "status" to the value of the data-element attribute and the value of the data-index attribute of the element that triggered the event
+  // This line of code is preventing the event from bubbling up the DOM tree
+  event.stopPropagation();
+}
+);
+
+document.addEventListener('click', function () {
+  document.querySelector('#status').textContent = 'container';
 });
+
