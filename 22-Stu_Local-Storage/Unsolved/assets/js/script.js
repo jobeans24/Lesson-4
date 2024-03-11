c2ffef869cb5f1196df550861a5dd80f244d29e8
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-03-11 18:50:41 -0400

    lesson 22 localStorage

--- 22-Stu_Local-Storage/Unsolved/assets/js/script.js
+++ 22-Stu_Local-Storage/Unsolved/assets/js/script.js
@@ -1,34 +1,48 @@
-var emailInput = document.querySelector("#email");
-var passwordInput = document.querySelector("#password");
-var signUpButton = document.querySelector("#sign-up");
-var msgDiv = document.querySelector("#msg");
-var userEmailSpan = document.querySelector("#user-email");
-var userPasswordSpan = document.querySelector("#user-password");
 
-renderLastRegistered();
+var emailInput = document.querySelector("#email"); 
+var passwordInput = document.querySelector("#password"); 
+var signUpButton = document.querySelector("#sign-up"); 
+var msgDiv = document.querySelector("#msg"); 
+var userEmailSpan = document.querySelector("#user-email"); 
+var userPasswordSpan = document.querySelector("#user-password"); 
 
-function displayMessage(type, message) {
+renderLastRegistered(); 
+
+function displayMessage(type, message) { 
   msgDiv.textContent = message;
   msgDiv.setAttribute("class", type);
-}
-
+} 
+ 
 function renderLastRegistered() {
-  // TODO: Retrieve the last email and password and render it to the page
+  var lastRegisteredEmail = localStorage.getItem("email");
+  var lastRegisteredPassword = localStorage.getItem("password");
+
+  if (lastRegisteredEmail && lastRegisteredPassword) {
+    userEmailSpan.textContent = lastRegisteredEmail;
+    userPasswordSpan.textContent = lastRegisteredPassword;
+  }
 }
 
-signUpButton.addEventListener("click", function(event) {
-  event.preventDefault();
-
-  var email = document.querySelector("#email").value;
-  var password = document.querySelector("#password").value;
+signUpButton.addEventListener("click", function(event) { // add an event listener to the sign up button
+  event.preventDefault(); // prevent the default action
+ 
+  var email = document.querySelector("#email").value; // get the email and password from the input
+  var password = document.querySelector("#password").value; // get the email and password from the input
 
   if (email === "") {
-    displayMessage("error", "Email cannot be blank");
+    displayMessage("error", "Email cannot be blank"); // display the message
   } else if (password === "") {
-    displayMessage("error", "Password cannot be blank");
+    displayMessage("error", "Password cannot be blank"); // display the message
   } else {
-    displayMessage("success", "Registered successfully");
+    displayMessage("success", "Registered successfully"); // display the message
 
-  // TODO: Save email and password to localStorage and render the last registered user
+    localStorage.setItem("email", email); // set the email and password to local storage
+    localStorage.setItem("password", password); // set the email and password to local storage
+
+    renderLastRegistered(); // render the last registered email and password
   }
 });
+  
+
+
+
